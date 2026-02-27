3795f32ebdb771791263d4a682c8761e214bf223
Cristiano-S <57049189+Cristiano-SRM@users.noreply.github.com>
2026-02-27 20:48:07 +0000

    atividade 1 fatorial

--- ativ_1.java
+++ ativ_1.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
 
+public class ativ_1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int valorInteiro, fatorial;
+
+        System.out.println("Insira um valor inteiro para calcular seu fatorial: ");
+        valorInteiro = input.nextInt();
+        fatorial = valorInteiro;
+
+        for( int x = valorInteiro; x>1; x-- ) {
+
+            valorInteiro = valorInteiro -1;
+            fatorial = fatorial * valorInteiro;
+        }
+
+        System.out.println("Resultado: " + fatorial);
+
+      input.close();
+    }
+}
